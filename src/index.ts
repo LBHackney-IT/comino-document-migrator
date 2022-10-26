ee124cebfa916952395b0c0d85a3fb9e6e17b19e
Andrew Clowes <andrew.clowes@madetech.com>
2022-10-26 12:11:53 +0100

    DOC-1103 Added throttling to file pipeline

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,8 @@
+import { Transform } from "stream";
 import { BlobServiceClient } from "@azure/storage-blob";
 import { config } from "./config";
 import { AzureBlobListStream } from "./storage";
+import { ThrottledTransformStream } from "./throttle";
 
 const blobServiceClient = new BlobServiceClient(config.azure.blobStorage.url);
 const containerClient = blobServiceClient.getContainerClient(
@@ -9,7 +11,13 @@ const containerClient = blobServiceClient.getContainerClient(
 const objectListStream = new AzureBlobListStream(containerClient, {
   pageSize: 3,
 });
+const transform = new Transform({
+  transform: (chunk, _encoding, callback) => callback(null, chunk),
+});
+const throttledStream = new ThrottledTransformStream(transform, {
+  uniformDistribution: true,
+});
 
-objectListStream.on("data", (result) => {
+objectListStream.pipe(throttledStream).on("data", (result) => {
   console.log(result.toString());
 });
